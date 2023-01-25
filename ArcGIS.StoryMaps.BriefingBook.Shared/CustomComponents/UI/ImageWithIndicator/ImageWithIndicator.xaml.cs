595ec33e680fc3a18f7f8ccfa7998e41ee35a465
Zelin Zhu <ZZhu@esri.com>
2023-01-24 16:22:19 -0800

    Added ImageWithIndicator component

---
+++ ArcGIS.StoryMaps.BriefingBook.Shared/CustomComponents/UI/ImageWithIndicator/ImageWithIndicator.xaml.cs
@@ -0,0 +1,44 @@
+﻿namespace ArcGIS.StoryMaps.BriefingBook.Shared.CustomComponents.UI
+{
+    public partial class ImageWithIndicator : Frame
+    {
+        public static readonly BindableProperty ShowIndicatorProperty = BindableProperty.Create("ShowIndicator", typeof(bool), typeof(Frame), false, propertyChanged: OnShowIndicatorChanged);
+
+        public bool ShowIndicator
+        {
+            get => (bool)GetValue(ShowIndicatorProperty);
+            set => SetValue(ShowIndicatorProperty, value);
+        }
+
+        public static readonly BindableProperty ImageSourceProperty = BindableProperty.Create("ImageSource", typeof(ImageSource), typeof(Frame), null, propertyChanged: OnImageSourceChanged);
+
+        public ImageSource ImageSource
+        {
+            get => (ImageSource)GetValue(ImageSourceProperty);
+            set => SetValue(ImageSourceProperty, value);
+        }
+
+        public CustomImage()
+        {
+            InitializeComponent();
+
+            ActivityIndicator.IsVisible = ShowIndicator;
+            Image.Source = null;
+        }
+
+        private static void OnShowIndicatorChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (CustomImage)bindable;
+
+            control.ActivityIndicator.IsVisible = (bool)newValue;
+        }
+
+        private static void OnImageSourceChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (CustomImage)bindable;
+
+            control.Image.Source = null;
+            control.Image.Source = (ImageSource)newValue;
+        }
+    }
+}
